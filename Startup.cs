26771b7844fbb7daa267ea12fcd2079614fd9dcb
Georgi Stoyanov Ovalov <eaageova@students.eaaa.dk>
2020-03-19 12:10:45 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BikeToGoRental.Startup))]
+namespace BikeToGoRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
